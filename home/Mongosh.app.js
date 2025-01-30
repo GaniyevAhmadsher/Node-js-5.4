ef28b33e260d9d4e4b64ec504f5196992940ff57
GanievAhmadsher <ganievahmadsher6205@gmail.com>
2025-01-30 11:50:57 +0500

    Node-js-5.4 -- Mongo.db-Composs

--- home/Mongosh.app.js
+++ home/Mongosh.app.js
@@ -1,17 +1,26 @@
-"1. Barcha o'yinlarni ko'rsatish";
-"db.games.find()";
+/*
+use game_store
+db.createCollection("users")
+db.createCollection("games")
+db.createCollection("purchases")
 
-"2. Narxi 20$ dan yuqori o'yinlar";
-"db.games.find({ price: { $gt: 20 } })";
+1. Barcha o'yinlarni ko'rsatish
+db.games.find()
 
-"3. Reyting bo'yicha saralash";
-"db.games.find().sort({ rating: -1 })";
+2. Narxi 20$ dan yuqori o'yinlar
+db.games.find({ price: { $gt: 20 } })
 
-"4. PC platformali o'yinlar";
-"db.games.find({ platforms: `PC` })";
+3. Reyting bo'yicha saralash
+db.games.find().sort({ rating: -1 })
 
-"5. Username va emaillar";
-"db.users.find({}, { username: 1, email: 1, _id: 0 })";
+4. PC platformali o'yinlar
+db.games.find({ platforms: `PC` })
 
-"6. Yosh yangilash";
-"db.users.updateOne({ username: `gamer123` },{ $set: { age: 21 } })";
+5. Username va emaillar
+db.users.find({}, { username: 1, email: 1, _id: 0 })
+
+6. Yosh yangilash
+db.users.updateOne({ username: `gamer123` },{ $set: { age: 21 } })
+
+
+*/
